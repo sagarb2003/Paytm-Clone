7a88f7c29c3ada1400002cd2e01ccc7d44535651
sagarb2003 <sagarssb2003@gmail.com>
2024-02-01 00:11:27 +0530

    added accounts schema and added some constraints to both user and accounts schema

--- backend/db.js
+++ backend/db.js
@@ -2,17 +2,53 @@ import mongoose from "mongoose";
 mongoose.connect(
   "mongodb+srv://sagarb2003:Sagar1505@cluster0.ak0eh8h.mongodb.net/paytm"
 );
-const {Schema}=mongoose;
+const { Schema } = mongoose;
 
-const userSchema= new Schema({
-    username:string,
-    firstName:string,
-    lastName:string,
-    password:string,
-})
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+    maxLength: 50,
+  },
+});
 
-const User= mongoose.model('User',userSchema);
+const accountSchema = new Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId, // Reference to User model
+    ref: "User",
+    required: true,
+  },
+  balance: {
+    type: Number,
+    required: true,
+  },
+});
 
-module.exports={
-    User
-}
+const Account = mongoose.model("Account", accountSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = {
+  User,
+  Account,
+};
