1c73c22c2b5fe0e496022eff08b75a89af183956
sagarb2003 <sagarssb2003@gmail.com>
2024-02-01 23:52:35 +0530

    added routes and setup the tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import SignUp from "./components/SignUp"
+import SignIn from './components/SignIn'
+import Dashboard from './components/Dashboard'
+import SendMoney from './components/SendMoney'
+
 
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignUp/>} />
+        <Route path="signin" element={<SignIn/>} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/send" element={<SendMoney/>} />
+      </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
